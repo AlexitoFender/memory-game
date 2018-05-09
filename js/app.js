2b2f1e95aa0d111a586da3b03ed547404d716242
Alex <alexitofender@gmail.com>
2018-05-09 18:11:58 -0400

    fix: Fix orthographic errors of array to shuffle

--- js/app.js
+++ js/app.js
@@ -9,18 +9,18 @@ const contentToShuffle = [
 'fa fa-diamond',
 'fa fa-paper-plane-o',
 'fa fa-paper-plane-o',
-'fa fa-paper-archor',
-'fa fa-paper-archor',
-'fa fa-paper-bolt',
-'fa fa-paper-bolt',
-'fa fa-paper-cube',
-'fa fa-paper-cube',
-'fa fa-paper-leaf',
-'fa fa-paper-leaf',
-'fa fa-paper-bicycle',
-'fa fa-paper-bicycle',
-'fa fa-paper-bomb',
-'fa fa-paper-bomb']
+'fa fa-anchor',
+'fa fa-anchor',
+'fa fa-bolt',
+'fa fa-bolt',
+'fa fa-cube',
+'fa fa-cube',
+'fa fa-leaf',
+'fa fa-leaf',
+'fa fa-bicycle',
+'fa fa-bicycle',
+'fa fa-bomb',
+'fa fa-bomb']
 
 /*
  * Display the cards on the page

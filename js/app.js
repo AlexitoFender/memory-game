6ed76f23f03b39aaf889fa8bd10ec765ae3c804f
Alex <alexitofender@gmail.com>
2018-05-09 18:18:16 -0400

    feat: Create function to add shuffled cards into deck

--- js/app.js
+++ js/app.js
@@ -29,6 +29,12 @@ const contentToShuffle = [
  *   - add each card's HTML to the page
  */
 
+function addShuffleCardContent(array){
+	for (let i = 0; i < 16; i++){
+		cardContentList[i].setAttribute('class', array[i]);
+	}
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;

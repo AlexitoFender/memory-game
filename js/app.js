4a2ff18103ba5ed24cd0ac8b1f060122c462a9be
Alex <alexitofender@gmail.com>
2018-05-11 21:46:37 -0400

    feat: Add animation flip cards out
    I made a change on the opacity in the animation library from 0 to 100

--- js/app.js
+++ js/app.js
@@ -176,6 +176,10 @@ function cardsDoNotMatch(array){
   		setTimeout(function(){
   		 	openList[0].setAttribute("class", "card");
   			openList[1].setAttribute("class", "card");
+        openList[0].classList.toggle("animated");
+        openList[0].classList.toggle("flipInY");
+        openList[1].classList.toggle("animated");
+        openList[1].classList.toggle("flipInY");
   			openList = [];
   			addEventListener();
   		}, 700);
